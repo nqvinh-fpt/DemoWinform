acf81179217ff091585cae15d59b31d40e1d8c53
NQVinh <128468164+nqvinh-fpt@users.noreply.github.com>
2025-01-05 16:03:52 +0700

    Add project HTMScraper

---
+++ HTMLScraper/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace HTMLScraper
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnScraper = new Button();
+            txtHtml = new TextBox();
+            SuspendLayout();
+            // 
+            // btnScraper
+            // 
+            btnScraper.Location = new Point(12, 12);
+            btnScraper.Name = "btnScraper";
+            btnScraper.Size = new Size(94, 29);
+            btnScraper.TabIndex = 0;
+            btnScraper.Text = "Lấy HTML";
+            btnScraper.UseVisualStyleBackColor = true;
+            btnScraper.Click += btnScraper_Click;
+            // 
+            // txtHtml
+            // 
+            txtHtml.Location = new Point(12, 47);
+            txtHtml.Multiline = true;
+            txtHtml.Name = "txtHtml";
+            txtHtml.ReadOnly = true;
+            txtHtml.ScrollBars = ScrollBars.Vertical;
+            txtHtml.Size = new Size(626, 582);
+            txtHtml.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(650, 641);
+            Controls.Add(txtHtml);
+            Controls.Add(btnScraper);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnScraper;
+        private TextBox txtHtml;
+    }
+}
---
+++ HTMLScraper/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using HtmlAgilityPack;
+using HtmlDocument = HtmlAgilityPack.HtmlDocument;
+
+namespace HTMLScraper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnScraper_Click(object sender, EventArgs e)
+        {
+            txtHtml.Text = ScrapeVnExpress();
+        }
+
+        private string ScrapeVnExpress()
+        {
+            string result = ""; 
+
+            var options = new ChromeOptions();
+            options.AddArgument("--headless"); // Chạy chế độ ẩn
+            options.AddArgument("--disable-gpu"); // Tắt GPU
+            options.AddArgument("--no-sandbox"); // Tăng tính ổn định
+            options.AddArgument("--disable-extensions"); // Tắt extension
+            options.AddArgument("--blink-settings=imagesEnabled=false"); 
+            options.AddArgument("--disable-javascript"); 
+            options.AddArgument("--disable-css"); 
+            options.AddArgument("--window-size=1920,1080"); // Cố định kích thước
+
+            using (IWebDriver driver = new ChromeDriver(options))
+            {
+                try
+                {
+                    driver.Navigate().GoToUrl("https://vnexpress.net/tin-tuc-24h");
+
+                    WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+                    wait.Until(drv => drv.FindElement(By.XPath("//h3[@class='title-news']/a")));
+
+                    var elements = driver.FindElements(By.XPath("//h3[@class='title-news']/a"));
+                    foreach (var element in elements)
+                    {
+                        string title = element.Text.Trim();
+                        string link = element.GetAttribute("href");
+
+                        result += $"Tiêu đề: {title}{Environment.NewLine}";
+                        result += $"Link: {link}{Environment.NewLine}";
+                        result += "----------------------------------------" + Environment.NewLine;
+                    }
+
+                    if (string.IsNullOrEmpty(result))
+                    {
+                        result = "Không tìm thấy bài viết nào.";
+                    }
+                }
+                catch (WebDriverTimeoutException)
+                {
+                    result = "Lỗi: Không thể tải phần tử cần thiết.";
+                }
+                catch (Exception ex)
+                {
+                    result = "Lỗi không xác định: " + ex.Message;
+                }
+            }
+
+            return result;
+        }
+    }
+}
